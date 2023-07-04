d5740b4cd07eb0abbeff6c2c64805d5d58b9ea18
BIROUE ISAAC <biroueisaac@gmail.com>
2023-07-04 23:10:37 +0100

    refactor my code

--- script.js
+++ script.js
@@ -18,25 +18,25 @@ button.addEventListener("click", (event) => {
   const book = new Book(titre, auteur, page);
   if (myLibrary.push(book)) {
     const div1 = document.createElement("div");
-    const delete_book = document.createElement('div')
-    const read = document.createElement('div')
-    read.classList.add('read')
-    read.textContent='Read'
-    delete_book.classList.add('delete_book')
-    delete_book.textContent="remove"
-    div1.append(delete_book,read)
+    const delete_book = document.createElement("div");
+    const read = document.createElement("div");
+    read.classList.add("read");
+    read.textContent = "Read";
+    delete_book.classList.add("delete_book");
+    delete_book.textContent = "remove";
+    div1.append(delete_book, read);
     div1.classList.add("grid-element");
     table.append(div1);
-    console.log(div1)
-    button.addEventListener('click',(e)=>{
-      e.preventDefault()
-      const supprimer2 = document.querySelectorAll('.delete_book')
-      supprimer2.forEach(function(element){
-        element.addEventListener('click',()=>{
-          element.parentNode.remove()
-        })
-      })
-      })
+    console.log(div1);
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const supprimer2 = document.querySelectorAll(".delete_book");
+      supprimer2.forEach(function (element) {
+        element.addEventListener("click", () => {
+          element.parentNode.remove();
+        });
+      });
+    });
   }
   const fields = document.querySelectorAll("input");
   fields.forEach(function (element) {
@@ -44,4 +44,3 @@ button.addEventListener("click", (event) => {
   });
 });
 //delete book from our bookstore
-
