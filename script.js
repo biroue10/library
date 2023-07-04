d9a3201b526480dbedf189940e7330e273505f67
BIROUE ISAAC <biroueisaac@gmail.com>
2023-07-04 20:57:55 +0100

    refactor my code

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Book(title, author, pages) {
     return `The ${title} by ${author}, ${pages} pages, not read yet`;
   };
 }
-const button = document.querySelector(".container button");
+const button = document.querySelector("#submit");
 const table = document.querySelector(".table");
 button.addEventListener("click", (event) => {
   event.preventDefault();
@@ -19,9 +19,12 @@ button.addEventListener("click", (event) => {
   if (myLibrary.push(book)) {
     const div1 = document.createElement("div");
     const delete_book = document.createElement('div')
+    const read = document.createElement('div')
+    read.classList.add('read')
+    read.textContent='Read'
     delete_book.classList.add('delete_book')
     delete_book.textContent="remove"
-    div1.append(delete_book)
+    div1.append(delete_book,read)
     div1.classList.add("grid-element");
     table.append(div1);
   }
